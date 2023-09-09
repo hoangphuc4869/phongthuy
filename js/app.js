f31583985dd65a274bcb2ff6b5c91173d9143247
hoangphuc4869 <phuchoangdo4869@gmail.com>
2023-09-09 11:45:43 +0700

    update

--- js/app.js
+++ js/app.js
@@ -12,3 +12,13 @@ var swiper = new Swiper(".mySwiper", {
   speed: 900,
   fadeEffect: { crossFade: true },
 });
+
+//share btn
+
+const link = encodeURI(window.location.href);
+const linkedIn = document.querySelector("#linkedIn");
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
+const fb = document.querySelector("#facebook");
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+const Print = document.querySelector("#print");
+Print.addEventListener("click", () => window.print());
